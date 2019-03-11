4ca04f362fb4e1ca99ff6a1343cbb3e19a1c3a9e
Str-Gen <laurens.dhooge@telenet.be>
2019-03-11 11:46:47 +0100

    +

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	atktools "gitlab.ilabt.imec.be/lpdhooge/containercap/attack-tools"
 	kubeapi "gitlab.ilabt.imec.be/lpdhooge/containercap/kube-api-interaction"
@@ -28,8 +29,11 @@ func main() {
 	nmap := atktools.NewNmap()
 	scn.Attacker.AtkCommand = strings.Join(nmap.BuildAtkCommand(), " ")
 	fmt.Println("launched: ", scn.Attacker.AtkCommand)
+	scn.StartTime = time.Now()
 	kubeapi.ExecShellInContainer("default", scn.UUID.String(), "nmap", scn.Attacker.AtkCommand)
 	kubeapi.DeletePod(scn.UUID.String())
+	scn.StopTime = time.Now()
+	scenario.WriteScenario(scn, file.Name())
 
 	file, err = os.Open("scenario2-test.yaml")
 	if err != nil {
@@ -42,8 +46,11 @@ func main() {
 	kubeapi.CreatePod(podspec)
 	scn.Attacker.AtkCommand = strings.Join(nmap.BuildAtkCommand(), " ")
 	fmt.Println("launched: ", scn.Attacker.AtkCommand)
+	scn.StartTime = time.Now()
 	kubeapi.ExecShellInContainer("default", scn.UUID.String(), "nmap", scn.Attacker.AtkCommand)
 	kubeapi.DeletePod(scn.UUID.String())
+	scn.StopTime = time.Now()
+	scenario.WriteScenario(scn, file.Name())
 
 	//kubeapi.WatchPod()
 	//kubeapi.ListPod()
