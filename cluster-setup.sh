6d2056ed8047d5eba103025b8a2dcaabd8c1ffdb
lpdhooge <laurens.dhooge@ugent.be>
2021-09-06 21:06:00 +0200

    mods for kubeadm cluster

--- cluster-setup.sh
+++ cluster-setup.sh
@@ -1,13 +1,20 @@
 #! /usr/bin/zsh
 
-# Prepare the nfs server
-# echo "/storage/nfs *(rw,insecure,sync,no_subtree_check,no_root_squash)" | sudo tee /etc/exports
+# Prepare the nfs server, do not expose this setup on the internet
+# the no_root_squash is a workaround so that the containers are allowed to write on the share
+echo "/storage/nfs/kube *(rw,insecure,sync,no_subtree_check,no_root_squash)" | sudo tee /etc/exports
 
 # Export nfs
-# sudo exportfs -rav
+sudo exportfs -rav
 
+# WARNING: this is a legacy line, it is here together with kind-example-config.yaml as an ultimate fallback to get going
 # Create Kubernetes cluster through kubernetes in docker
-kind create cluster --config kind-example-config.yaml
+# kind create cluster --config kind-example-config.yaml
+# Make sure that the tools to mount properly are installed in the nodes
+# docker exec kind-worker bash -c "apt update && apt install -y nfs-common" &>/dev/null &
+# docker exec kind-worker2 bash -c "apt update && apt install -y nfs-common" &>/dev/null &
+# docker exec kind-worker3 bash -c "apt update && apt install -y nfs-common" &>/dev/null &
+
 
 # Create PersistentVolume and PersistentVolumeClaim
 kubectl create -f pv-nfs.yaml
@@ -16,8 +23,4 @@ kubectl create -f pvc-nfs.yaml
 # Create the IDLab secret
 kubectl create secret generic idlab-gitlab --from-file=.dockerconfigjson=/home/dhoogla/.docker/config.json --type=kubernetes.io/dockerconfigjson
 
-# Make sure that the tools to mount properly are installed in the nodes
-docker exec kind-worker bash -c "apt update && apt install -y nfs-common" &>/dev/null &
-docker exec kind-worker2 bash -c "apt update && apt install -y nfs-common" &>/dev/null &
-docker exec kind-worker3 bash -c "apt update && apt install -y nfs-common" &>/dev/null &
 
