21a484b1fe861a0218ecc1af181df9ce78270176
Str-Gen <laurens.dhooge@telenet.be>
2021-09-29 12:17:03 +0200

    modified setup files

--- kubeadm-teardown.sh
+++ kubeadm-teardown.sh
@@ -2,5 +2,5 @@ kubectl drain NODENAME --delete-local-data --force --ignore-daemonsets
 # Main clean-up
 sudo kubeadm reset
 # As stated after main clean-up, some components have to be removed manually
-sudo rm -rf /etc/cni/net.d
+sudo rm -f /etc/cni/net.d/*
 rm $HOME/.kube/config
