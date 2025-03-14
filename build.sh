b3a24eaba7093bd3e8955a4f6aed855732580711
Miel Verkerken <miel.verkerken@ugent.be>
2025-03-14 10:48:56 +0100

    fix  build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Building Concap..."
-cd cmd/concap
-go build -o ../../concap
-cd ../..
+cd cmd
+go build -o ../concap
+cd ..
 echo "Build completed successfully. Binary is at ./concap" 
